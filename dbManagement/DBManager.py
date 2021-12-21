8f7418a2816893f6d5f6686cf7df71d9cbd9c4a9
ThomasBernard28 <thomas.bernard@student.umons.ac.be>
2021-12-21 15:19:27 +0100

    add test version

--- dbManagement/DBManager.py
+++ dbManagement/DBManager.py
@@ -37,19 +37,20 @@ class DBManager:
             getTablesQuery = """SELECT name FROM sqlite_master WHERE type='table';"""
             self.cur.execute(getTablesQuery)
             records = self.cur.fetchall()
-
+            self.cur = self.conn.cursor()
             if dfTableName in records:
                 self.cur.execute("INSERT INTO " + dfTableName + " (tableName, lhs, rhs) VALUES (?,?,?)", (tableName, lhs, rhs))
                 self.conn.commit()
 
             else:
-                self.cur.execute("CREATE TABLE dfTable (tableName TEXT, lhs TEXT, rhs TEXT)")
-                self.cur.execute("INSERT INTO dfTable (tableName, lhs, rhs) VALUES (?,?,?)", (tableName, lhs, rhs))
+                createTableQuery = '''CREATE TABLE {}(tableName TEXT, lhs TEXT, rhs TEXT)'''.format(dfTableName)
+                self.cur.execute(createTableQuery)
+                self.cur.execute("INSERT INTO "+ dfTableName + " (tableName, lhs, rhs) VALUES (?,?,?)", (tableName, lhs, rhs))
                 self.conn.commit()
                 print("Your DF was successfully added to the dfTable in your database")
 
         except Error as error :
-            print("Failed to add your DF, syntax might be incorect please be sure to enter  : tableName \n lhs1 lhs2 lhsn \n rhs")
+            print("Failed to add your DF, syntax might be incorect please be sure to enter  : \n dfTableName \n tableName \n lhs1 lhs2 lhsn \n rhs")
         
 
     def deleteDF(self, dfTableName, tableName, lhs, rhs):
