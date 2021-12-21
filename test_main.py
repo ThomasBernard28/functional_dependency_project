26c0c7fb68aa94fbf099e4719f906e5e9fa2ae0a
Harbiinger <theogodin51@gmail.com>
2021-12-21 22:20:55 +0100

    test_main fixed

---
+++ test_main.py
@@ -0,0 +1,15 @@
+import pytest
+from dbManagement.DBManager import *
+
+def test_addDF():
+    """This also test getAllDF() method"""
+    dbm = DBManager("DATABANANA")
+    dbm.addDF("TestTable", "Testlhs1 Testlhs2", "Testrhs")
+    assert ("TestTable", "Testlhs1 Testlhs2", "Testrhs") in dbm.getAllDF("TestTable")
+    dbm.disconnect()
+
+def test_deleteDF():
+    dbm = DBManager("DATABANANA")
+    dbm.deleteDF("TestTable", "Testlhs1 Testlhs2", "Testrhs")
+    assert not ("TestTable", "Testlhs1 Testlhs2", "Testrhs") in dbm.showTable("FuncDep")
+    dbm.disconnect()
